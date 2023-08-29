5622fb6257daa0506c850bb5df383d5ead437521
kimbongjune <86637070+kimbongjune@users.noreply.github.com>
2023-08-29 21:17:42 +0900

    Update update_readme.py

--- update_readme.py
+++ update_readme.py
@@ -1,23 +1,21 @@
-import os
+import sys
 
-def update_readme_with_new_md_file(new_md_file_path):
-    # Extract info from the new MD file path
-    filename = os.path.basename(new_md_file_path)
-    display_name = filename.replace(".md", "").replace("_", " ")
-    link = f"- [[{display_name}]]({new_md_file_path})\n"
-
-    # Read the existing README
+def update_readme(changed_files):
     with open("README.md", "r") as f:
         lines = f.readlines()
 
-    # Append the new link at the end of README.md
-    lines.append(link)
+    for changed_file in changed_files:
+        link_name = "_".join(changed_file.split("_")[1:]).replace(".md", "")
+        link = f"- [[{link_name}]]({changed_file})\n"
 
-    # Write back to README
+        if link in lines:
+            lines.remove(link)
+        else:
+            lines.append(link)
+
     with open("README.md", "w") as f:
         f.writelines(lines)
 
 if __name__ == "__main__":
-    # Replace with the actual new MD file path when running through GitHub Actions
-    new_md_file_path = "your_new_md_file_path_here.md"
-    update_readme_with_new_md_file(new_md_file_path)
+    changed_files = sys.argv[1:]
+    update_readme(changed_files)
