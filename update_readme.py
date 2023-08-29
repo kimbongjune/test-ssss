3e3cc3196d69ed696e6017617e7d93196275cdfe
kimbongjune <86637070+kimbongjune@users.noreply.github.com>
2023-08-29 21:44:49 +0900

    Update update_readme.py

--- update_readme.py
+++ update_readme.py
@@ -1,14 +1,24 @@
+import sys
 import os
 
-def add_link_to_readme():
-    commit_files = os.getenv('GITHUB_SHA').split(",")
+def update_readme(merged, pr_title):
+    if not merged:
+        return
+
+    added_files = [f for f in os.listdir() if f.endswith('.md') and pr_title in f]
+    
+    if not added_files:
+        return
+    
+    added_file = added_files[0]
+    date, title = added_file.split("_", 1)
+    title = title.replace(".md", "")
+    new_link = f"- [[{date}] {title}](https://github.com/{GITHUB_REPOSITORY}/blob/main/{added_file})\n"
+
     with open("README.md", "a") as f:
-        for file in commit_files:
-            if file.endswith(".md"):
-                path = file
-                link_name = " ".join(path.split("_")[1:]).replace(".md", "")
-                link = f"- [[{link_name}]](https://github.com/${{ github.repository }}/blob/main/{path})"
-                f.write(link + "\n")
+        f.write(new_link)
 
 if __name__ == "__main__":
-    add_link_to_readme()
+    merged = sys.argv[1] == 'true'
+    pr_title = sys.argv[2]
+    update_readme(merged, pr_title)
