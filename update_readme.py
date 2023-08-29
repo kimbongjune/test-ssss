918c0e2864a7567d075869ae9cbc30d5622a8c38
kimbongjune <86637070+kimbongjune@users.noreply.github.com>
2023-08-29 21:50:50 +0900

    Update update_readme.py

--- update_readme.py
+++ update_readme.py
@@ -1,24 +1,14 @@
-import sys
 import os
 
-def update_readme(merged, pr_title):
-    if not merged:
-        return
-
-    added_files = [f for f in os.listdir() if f.endswith('.md') and pr_title in f]
-    
-    if not added_files:
-        return
-    
-    added_file = added_files[0]
-    date, title = added_file.split("_", 1)
-    title = title.replace(".md", "")
-    new_link = f"- [[{date}] {title}](https://github.com/{GITHUB_REPOSITORY}/blob/main/{added_file})\n"
-
+def update_readme(changed_files):
     with open("README.md", "a") as f:
-        f.write(new_link)
+        for changed_file in changed_files:
+            if changed_file.endswith('.md'):
+                date, title = changed_file.split("_", 1)
+                title = title.replace(".md", "")
+                new_link = f"- [[{date}] {title}](https://github.com/kimbongjune/TIL/blob/main/{changed_file})\n"
+                f.write(new_link)
 
 if __name__ == "__main__":
-    merged = sys.argv[1] == 'true'
-    pr_title = sys.argv[2]
-    update_readme(merged, pr_title)
+    changed_files = os.environ['CHANGED_FILES'].split()
+    update_readme(changed_files)
