b7f0921de55ec3bee8313cc16cb2f1cb548d9aae
kimbongjune <86637070+kimbongjune@users.noreply.github.com>
2023-08-29 21:58:35 +0900

    Update update_readme.py

--- update_readme.py
+++ update_readme.py
@@ -1,15 +1,19 @@
 import sys
 import os
 
-def update_readme(changed_files):
-    for added_file in changed_files:
+def update_readme(added_files):
+    if not added_files:
+        return
+
+    for added_file in added_files.split(','):
         if added_file.endswith('.md'):
             date, title = added_file.split("_", 1)
             title = title.replace(".md", "")
-            new_link = f"- [[{date}] {title}](https://github.com/{os.environ['GITHUB_REPOSITORY']}/blob/main/{added_file})\n"
+            new_link = f"- [[{date}] {title}](https://github.com/kimbongjune/TIL/blob/main/{added_file})\n"
+            
             with open("README.md", "a") as f:
                 f.write(new_link)
 
 if __name__ == "__main__":
-    changed_files = sys.argv[1].split(',')
-    update_readme(changed_files)
+    added_files = sys.argv[1]
+    update_readme(added_files)
