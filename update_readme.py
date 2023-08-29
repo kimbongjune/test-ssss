7a2691f2994ed83c877a8060ffa4724fb574555a
kimbongjune <86637070+kimbongjune@users.noreply.github.com>
2023-08-29 22:03:34 +0900

    Update update_readme.py

--- update_readme.py
+++ update_readme.py
@@ -1,19 +1,20 @@
-import sys
 import os
+import subprocess
 
-def update_readme(added_files):
-    if not added_files:
+def update_readme():
+    last_commit_files = subprocess.getoutput('git diff --name-only HEAD~1..HEAD').split("\n")
+    added_file = [f for f in last_commit_files if f.endswith('.md')]
+
+    if not added_file:
         return
+    
+    added_file = added_file[0]
+    date, title = added_file.split("_", 1)
+    title = title.replace(".md", "")
+    new_link = f"- [[{date}] {title}](https://github.com/kimbongjune/TIL/blob/main/{added_file})\n"
 
-    for added_file in added_files.split(','):
-        if added_file.endswith('.md'):
-            date, title = added_file.split("_", 1)
-            title = title.replace(".md", "")
-            new_link = f"- [[{date}] {title}](https://github.com/kimbongjune/TIL/blob/main/{added_file})\n"
-            
-            with open("README.md", "a") as f:
-                f.write(new_link)
+    with open("README.md", "a") as f:
+        f.write(new_link)
 
 if __name__ == "__main__":
-    added_files = sys.argv[1]
-    update_readme(added_files)
+    update_readme()
