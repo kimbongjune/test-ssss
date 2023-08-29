ec4a73f3d6fbd5c98305dbd726665a94bd2eca81
kimbongjune <86637070+kimbongjune@users.noreply.github.com>
2023-08-29 21:38:30 +0900

    Update update_readme.py

--- update_readme.py
+++ update_readme.py
@@ -1,26 +1,14 @@
 import os
-import sys
 
-def get_changed_md_files():
-    return [f for f in sys.argv[1:] if f.endswith('.md')]
-
-def update_readme(changed_files):
-    with open("README.md", "r") as f:
-        lines = f.readlines()
-
-    for changed_file in changed_files:
-        file_name = os.path.basename(changed_file)
-        link_name = "_".join(file_name.split("_")[1:]).replace(".md", "")
-        link = f"- [[{link_name}]]({changed_file})\n"
-
-        if link in lines:
-            lines.remove(link)
-        else:
-            lines.append(link)
-
-    with open("README.md", "w") as f:
-        f.writelines(lines)
+def add_link_to_readme():
+    commit_files = os.getenv('GITHUB_SHA').split(",")
+    with open("README.md", "a") as f:
+        for file in commit_files:
+            if file.endswith(".md"):
+                path = file
+                link_name = " ".join(path.split("_")[1:]).replace(".md", "")
+                link = f"- [[{link_name}]](https://github.com/${{ github.repository }}/blob/main/{path})"
+                f.write(link + "\n")
 
 if __name__ == "__main__":
-    changed_files = get_changed_md_files()
-    update_readme(changed_files)
+    add_link_to_readme()
