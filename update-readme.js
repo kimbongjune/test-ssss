ea14de8852c0ded4f06833b90be03706577dfa46
kimbongjune <86637070+kimbongjune@users.noreply.github.com>
2023-08-30 11:39:37 +0900

    Update update-readme.js

--- update-readme.js
+++ update-readme.js
@@ -4,55 +4,43 @@ const { execSync } = require('child_process');
 
 const repoName = process.env.GITHUB_REPOSITORY;
 
-const isMergedMdFile = (filename) => {
-  const regex = /^\d{4}-\d{2}-\d{2}_.*\.md$/;
-  return regex.test(filename);
+const getMdFiles = (type) => {
+  const stdout = execSync(`git diff --name-only --diff-filter=${type} HEAD~ HEAD`).toString();
+  return stdout.split('\n').filter(f => f.endsWith('.md'));
 };
 
-const generateLink = (filename) => {
-  const date = filename.substring(0, 10);
-  const title = decodeURIComponent(filename.substring(11, filename.length - 3));
-  return `- [[${date}] ${title}](https://github.com/${repoName}/blob/main/${filename.replace(/ /g, '%20')})`;
-};
+const encodeLink = (filename) => encodeURIComponent(filename);
+const decodeLink = (filename) => decodeURIComponent(filename);
 
-const mainBranch = 'main';
 const readmePath = path.join(__dirname, 'README.md');
 
-const deletedFiles = execSync('git diff HEAD~1 HEAD --name-status', { encoding: 'utf-8' })
-  .split('\n')
-  .filter(line => line.startsWith('D'))
-  .map(line => line.slice(2).trim());
-
-fs.readdir('.', (err, filenames) => {
+fs.readFile(readmePath, 'utf8', (err, readmeContent) => {
   if (err) {
     console.error(err);
     return;
   }
 
-  let readmeContent = fs.readFileSync(readmePath, 'utf8');
+  let newReadmeContent = readmeContent;
 
-  // 파일 추가
-  const mergedMdFiles = filenames.filter(isMergedMdFile);
-  mergedMdFiles.forEach((filename) => {
-    const link = generateLink(filename);
-    if (!readmeContent.includes(link)) {
-      readmeContent += `\n${link}`;
+  const deletedMdFiles = getMdFiles('D');
+  deletedMdFiles.forEach((filename) => {
+    const link = `- [[${filename.substring(0, 10)}] ${decodeLink(filename.substring(11, filename.length - 3))}](https://github.com/${repoName}/blob/main/${encodeLink(filename)})`;
+    newReadmeContent = newReadmeContent.replace(link, '');
+  });
+
+  const addedMdFiles = getMdFiles('A');
+  addedMdFiles.forEach((filename) => {
+    const link = `- [[${filename.substring(0, 10)}] ${decodeLink(filename.substring(11, filename.length - 3))}](https://github.com/${repoName}/blob/main/${encodeLink(filename)})`;
+    if (!newReadmeContent.includes(link)) {
+      newReadmeContent += `\n${link}`;
     }
   });
 
-  // 파일 삭제
-  deletedFiles.forEach((filename) => {
-    if (isMergedMdFile(filename)) {
-      const link = generateLink(filename);
-      const regex = new RegExp(`\n${link}`);
-      readmeContent = readmeContent.replace(regex, '');
-    }
-  });
-
-  fs.writeFileSync(readmePath, readmeContent, 'utf8');
-  execSync(`git config user.name "GitHub Action"`);
-  execSync(`git config user.email "action@github.com"`);
+  fs.writeFileSync(readmePath, newReadmeContent, 'utf8');
+  
+  execSync('git config user.name "GitHub Action"');
+  execSync('git config user.email "action@github.com"');
   execSync(`git add ${readmePath}`);
-  execSync(`git commit -m "Update README.md"`);
+  execSync('git commit -m "Update README.md"');
   execSync(`git push https://${process.env.GITHUB_TOKEN}@github.com/${repoName}.git ${mainBranch}`);
 });
