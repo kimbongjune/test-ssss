9c8f9f71949a7172bddceef70930d50e3bdc6be2
kimbongjune <86637070+kimbongjune@users.noreply.github.com>
2023-09-02 18:08:36 +0900

    Update update_readme.js

--- update_readme.js
+++ update_readme.js
@@ -1,58 +1,38 @@
-const fs = require('fs');
-const path = require('path');
-const execSync = require('child_process').execSync;
+const axios = require('axios');
 
-function fetchGitTrackedFiles() {
-  const output = execSync('git ls-files', { encoding: 'utf8' });
-  return new Set(output.split('\n').filter(Boolean));
-}
+const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/');
 
-function fetchStructure(dir, gitTrackedFiles) {
-  let structure = [];
-  
-  const files = fs.readdirSync(dir);
-  for (const file of files) {
-    if (file === '.git' || file === 'node_modules' || file === '.github') continue;
-    const filePath = path.join(dir, file).replace(/\\/g, '/');
-    const relativePath = filePath.replace(/^\.\//, '');
-    
-    if (!gitTrackedFiles.has(relativePath) && !fs.statSync(filePath).isDirectory()) {
-      continue;
+async function fetchGithubRepoStructure() {
+  try {
+    const { data } = await axios.get(`https://api.github.com/repos/${owner}/${repo}/git/trees/main?recursive=1`);
+    let directories = {};
+    let output = "";
+
+    for (const item of data.tree) {
+      const splitPath = item.path.split('/');
+      let subDir = directories;
+
+      for (let i = 0; i < splitPath.length; i++) {
+        const part = splitPath[i];
+
+        if (!subDir[part]) {
+          if (i === splitPath.length - 1 && item.type === 'blob') {
+            output += ' '.repeat(i * 2) + `📄 ${part}\n`;
+          } else {
+            output += ' '.repeat(i * 2) + `📂 ${part}\n`;
+            subDir[part] = {};
+          }
+        }
+
+        subDir = subDir[part];
+      }
     }
 
-    const stats = fs.statSync(filePath);
-    
-    if (stats.isDirectory()) {
-      structure.push({
-        type: 'directory',
-        name: file,
-        children: fetchStructure(filePath, gitTrackedFiles)
-      });
-    } else {
-      structure.push({
-        type: 'file',
-        name: file
-      });
-    }
-  }
-  
-  return structure;
-}
+    console.log(output);
 
-function printStructure(arr, level = 0) {
-  for (let i = 0; i < arr.length; i++) {
-    const item = arr[i];
-    const prefix = ' '.repeat(level * 2);
-    
-    if (item.type === 'directory') {
-      console.log(`${prefix}📂 ${item.name}`);
-      printStructure(item.children, level + 1);
-    } else {
-      console.log(`${prefix}📄 ${item.name}`);
-    }
+  } catch (err) {
+    console.error(err);
   }
 }
 
-const gitTrackedFiles = fetchGitTrackedFiles();
-const directoryStructure = fetchStructure('.', gitTrackedFiles);
-printStructure(directoryStructure);
+fetchGithubRepoStructure();
